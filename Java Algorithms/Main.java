988f4664774137c297a1302746557ccb21d8e7ff
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-09-05 14:26:25 -0400

    Inheritance practice

--- Java Algorithms/Main.java
+++ Java Algorithms/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Email email1 = new Email("Richies", "Luong");
-        System.out.println(email1.showInfo());
+        Mouse1 m1 = new Mouse1();
+        m1.leftClick();
+        m1.rightClick();
+        System.out.println(m1.texture);
+        m1.scrollDown();
+        m1.scrollUp();
+
+        Mouse2 m2 = new Mouse2();
+        m2.connectBluetooth();
+
     }
 }
