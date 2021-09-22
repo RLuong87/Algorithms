5df1e66a3e8c46d9ef6b7e738aefb2e0343d4dd5
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-09-22 14:47:32 -0400

    Add files via upload

--- Java Algorithms/FindLargestNums.java
+++ Java Algorithms/FindLargestNums.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FindLargestNums {
 
     /*
+    Edabit Challenge
+
     Create a function that takes an array of arrays with numbers.
     Return a new (single) array with the largest numbers of each
 
@@ -15,24 +19,27 @@ public class FindLargestNums {
     findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
     */
 
-    public static double[] findLargestNums(double[][] arr) {
-
-        double[] numbers;
-        int index = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            numbers = new double[arr.length];
-            System.out.println(Arrays.toString(numbers));
-
-        }
+    public static int[] findLargestNums(double[][] arr) {
 
         return null;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(findLargestNums(new double[][]{{4, 2, 7, 1}, {20, 70, 40, 90}, {1, 2, 0}})));
+        // Create a list to place the biggest numbers when found
+        List<Integer> biggestNumbers = new ArrayList<>();
+        int[][] numbers = {{4, 2, 7, 1}, {20, 70, 40, 90}, {1, 2, 0}};
 
+        for (int[] number : numbers) {
+            int numCheck = number[0];
+
+            for (int num : number) {
+                if (num > numCheck) {
+                    numCheck = num;
+                }
+            }
+            biggestNumbers.add(numCheck);
+        }
+        System.out.println(biggestNumbers);
     }
 }
