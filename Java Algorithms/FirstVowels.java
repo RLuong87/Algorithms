94b8e784339dd001a595224118d1799a7ac12d6e
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-09-27 11:57:13 -0400

    Add files via upload

---
+++ Java Algorithms/FirstVowels.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class FirstVowels {
+
+    /*
+    Edabit Challenge
+
+    Write a function that returns the first n vowels of a string.
+
+    Examples:
+    firstNVowels("sharpening skills", 3) ➞ "aei"
+
+    firstNVowels("major league", 5) ➞ "aoeau"
+
+    firstNVowels("hostess", 5) ➞ "invalid"
+
+    Notes:
+    Return "invalid" if the n exceeds the number of vowels in a string.
+    Vowels are: a, e, i, o, u
+    */
+
+    public static String firstNVowels(String str, int n) {
+
+        String vowel = "aeiou";
+        String vow = "";
+
+        for (int i = 0; i < n; i++) {
+            
+            if (str.length() > n) {
+
+            }
+        }
+
+        return "";
+    }
+}
