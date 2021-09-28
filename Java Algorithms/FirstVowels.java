fc774fca06700095ab3de38fc6a929bfc06cf3c3
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-09-27 22:53:31 -0400

    Not a working solution yet

--- Java Algorithms/FirstVowels.java
+++ Java Algorithms/FirstVowels.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FirstVowels {
 
     /*
@@ -22,15 +26,42 @@ public class FirstVowels {
     public static String firstNVowels(String str, int n) {
 
         String vowel = "aeiou";
+        char[] chars = str.toCharArray(); // Look at each string
         String vow = "";
 
         for (int i = 0; i < n; i++) {
-            
-            if (str.length() > n) {
 
+            if (chars.length == n && chars.length == vowel.length()) {
+                vow += chars[i];
+                return vow;
             }
         }
+        return "invalid";
+    }
 
-        return "";
+    public static void main(String[] args) {
+
+//        System.out.println(firstNVowels("sharpening skills", 3));
+        int num = 5;
+        String vowel = "aeiou";
+        String str = "sharpening skills";
+        String[] vowArray = vowel.split("");
+        String[] newStr = str.split(" ");
+
+        for (int i = 0; i < newStr.length; i++) {
+            char chars = newStr[i].charAt(i);
+
+            if (chars == 'a') {
+                newStr[i] = newStr[i].substring(0, num);
+            }
+        }
+        StringBuilder vow = new StringBuilder();
+
+        for (String s : newStr) {
+            vow.append(s);
+        }
+        System.out.println(vow);
+        System.out.println(Arrays.toString(vowArray));
+        System.out.println(Arrays.toString(newStr));
     }
 }
