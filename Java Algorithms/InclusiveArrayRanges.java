cdd662143a8a9d97a47df460c87ef48227e8f4be
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-10-01 21:06:13 -0400

    Add files via upload

---
+++ Java Algorithms/InclusiveArrayRanges.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class InclusiveArrayRanges {
+
+    /*
+    Edabit Challenges
+
+    Write a function that, given the start startNum and end endNum values, return an array containing all the numbers inclusive to that range.
+    See examples below.
+
+    Examples:
+    inclusiveArray(1, 5) ➞ [1, 2, 3, 4, 5]
+
+    inclusiveArray(2, 8) ➞ [2, 3, 4, 5, 6, 7, 8]
+
+    inclusiveArray(10, 20) ➞ [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+    inclusiveArray(17, 5) ➞ [17]
+
+    Notes:
+    The numbers in the array are sorted in ascending order.
+    If startNum is greater than endNum, return an array with the higher value. See example #4.
+    */
+
+    public static int[] inclusiveArray(int startNum,int endNum) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        int[] inNums = { 1, 5 };
+
+        for (int i = 0; i < inNums.length; i++) {
+            System.out.println(inNums[0] + inNums[1] + " " + (i + 1));
+        }
+    }
+}
