a5719fc7689dd9f1026176ba5c8db6ef62043f05
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-10-13 22:31:29 -0400

    Add files via upload

---
+++ Java Algorithms/FindTheMissingNumber.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class FindTheMissingNumber {
+
+    /*
+    Edabit Challenge
+
+    Create a method that takes an array of integers between 1 and 10 (excluding one number) and returns the missing number.
+
+    Examples:
+    missingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+
+    missingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+
+    missingNum([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+
+    Notes:
+    The array of numbers will be unsorted (not in order).
+    Only one number will be missing
+    */
+
+    public static int missingNum(int[] nums) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
