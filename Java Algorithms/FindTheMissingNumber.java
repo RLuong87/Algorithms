13897635fd25b81846e22f03c9dfd343a6f9e5cd
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-10-14 09:55:17 -0400

    Add files via upload

--- Java Algorithms/FindTheMissingNumber.java
+++ Java Algorithms/FindTheMissingNumber.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class FindTheMissingNumber {
 
     /*
@@ -21,12 +26,32 @@ public class FindTheMissingNumber {
 
     public static int missingNum(int[] nums) {
 
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+
         return 0;
     }
 
     public static void main(String[] args) {
 
+        int[] numbers = {10, 5, 1, 2, 4, 6, 8, 3, 9};
+//        Arrays.sort(numbers);
+        Arrays.sort(numbers, 1, 4);
+        System.out.println(Arrays.toString(numbers));
+        System.out.printf("Modified array[]: %s (In numerical order from index 1 to index 5)", Arrays.toString(numbers));
+
+        // Note: Integer[] instead of int[], Collections.reverseOrder
+        // doesn't work for primitive types
+        Integer[] numArr = {7, 2, 3, 6, 5, 9, 1, 4, 8};
+        // Sorts numArr[] in descending order using reverseOrder() of Collections class
+        // in Arrays.sort() as an argument
+        Arrays.sort(numArr, Collections.reverseOrder());
+        System.out.printf("\nModified array[]: %s (In descending order)", Arrays.toString(numArr));
 
 
+        String[] array = {"google.com", "facebook.com", "github.com"};
+        Arrays.sort(array);
+        System.out.printf("\nModified array[]: %s (In alphabetical order)", Arrays.toString(array));
     }
 }
