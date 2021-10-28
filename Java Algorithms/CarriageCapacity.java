08f8ee4a5d7bbc42d57c6aa9d66d78c3c22ebe1a
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-10-28 13:50:24 -0400

    Add files via upload

--- Java Algorithms/CarriageCapacity.java
+++ Java Algorithms/CarriageCapacity.java
@@ -3,7 +3,6 @@ package com.company;
 public class CarriageCapacity {
 
     /*
-    
     Edabit Challenge
 
     A train has a maximum capacity of n passengers overall, which means each carriage's capacity will share an equal proportion of the maximum capacity.
@@ -30,24 +29,52 @@ public class CarriageCapacity {
     All carriage numbers will be positive integers, which will be able to divide evenly.
     Remember to return -1 if no carriage is empty enough.
     */
+
     public static int findSeat(int n, int[] arr) {
-        int capacity = (n / arr.length);
+        int maximumCap = (n / arr.length); // First find the maximum capacity for each carriage, n divided by the number of carriages
 
         for (int i = 0; i < arr.length; i++) {
+            double percent = (double) arr[i] / maximumCap * 100; // Find the percentage of each carriage
 
-            if (n > capacity) {
-                return i;
-            } else {
-                return -1;
+            if (percent <= 50) { // if the percentage is 50 or less
+                return i; // returns the index when the first carriage is found
             }
         }
-        return 0;
+        return -1; // return -1 if the carriage doesn't exist
     }
 
     public static void main(String[] args) {
 
-        int[] cap = {35, 23, 40, 21, 38};
-        System.out.println(findSeat(200, cap));
+        int[] capArr = {50, 20, 80, 90, 100, 60, 30, 50, 80, 60};
+        int[] capArr2 = {35, 23, 18, 10, 40};
+        int[] capArr3 = {35, 23, 40, 21, 38};
+        int[] capArr4 = {3, 5, 4, 2};
+        System.out.println(findSeat(1000, capArr));
+        System.out.println(findSeat(200, capArr2));
+        System.out.println(findSeat(200, capArr3));
+        System.out.println(findSeat(20, capArr4));
 
+//        System.out.println((double) 35 / 40 * 100);
+
+        int number = 1000;
+        int maxCap = number / capArr.length;
+        int index = 0, full = 0;
+
+        for (int i = 0; i < capArr.length; i++) {
+            double percentCap = (double) capArr[i] / maxCap * 100;
+//            System.out.println(i + " " + capArr[i] + " " + (percentCap));
+
+            if (percentCap <= 50) {
+                index = i;
+//                System.out.println(i);
+            } else {
+                full = -1;
+//                System.out.println(-1);
+            }
+
+        }
+//        System.out.println(index);
+//        System.out.println(full);
+//        System.out.println(maxCap);
     }
 }
