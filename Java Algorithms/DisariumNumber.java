37de0a3f72a7d58d5d54bffc89fe15f953209e0f
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-11-03 10:54:42 -0400

    CH14 - Warmup Java Algorithms - Challenge Set 3

---
+++ Java Algorithms/DisariumNumber.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DisariumNumber {
+
+    /*
+    Edabit Challenge
+    https://edabit.com/challenge/GjCgCugvh4eorPmgM
+
+    A number is said to be Disarium if the sum of its digits raised to their respective positions is the number itself.
+
+    Create a function that determines whether a number is a Disarium or not.
+
+    Examples:
+    isDisarium(75) ➞ false
+    // 7^1 + 5^2 = 7 + 25 = 32
+
+    isDisarium(135) ➞ true
+    // 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+
+    isDisarium(518) ➞ true
+
+    isDisarium(544) ➞ false
+
+    isDisarium(8) ➞ true
+
+    isDisarium(466) ➞ false
+
+    Notes:
+    Position of the digit is 1-indexed.
+    A recursive version of this challenge can be found via this link.
+     */
+
+    public static boolean isDisarium(int n) {
+
+        ArrayList<Integer> numsList = new ArrayList<>();
+        ArrayList<Integer> numsArr = new ArrayList<>();
+        String strNumArr = String.valueOf(n);
+        String[] numStr = strNumArr.split("");
+        int numPow, num = 0;
+
+        for (String s : numStr) {
+            numsList.add(Integer.parseInt(s));
+        }
+        for (int i = 0; i < numsList.size(); i++) {
+            numPow = (int) Math.pow(numsList.get(i), i + 1);
+            numsArr.add(numPow);
+        }
+        for (Integer integer : numsArr) {
+            num += integer;
+
+            if (num == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isDisarium(75));
+        System.out.println(isDisarium(135));
+        System.out.println(isDisarium(518));
+        System.out.println(isDisarium(544));
+        System.out.println(isDisarium(8));
+        System.out.println(isDisarium(466));
+
+//        System.out.println(Math.pow(1, 1) + Math.pow(3, 2) + Math.pow(5, 3));
+//        System.out.println(Math.pow(7, 1) + Math.pow(5, 2));
+//        System.out.println(Math.pow(5, 3));
+    }
+}
