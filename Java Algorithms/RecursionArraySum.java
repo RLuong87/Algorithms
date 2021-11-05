49336e36e139bc242c7eef516effc9fe67ce4062
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-11-04 20:54:07 -0400

    sum of array with recursion

---
+++ Java Algorithms/RecursionArraySum.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class RecursionArraySum {
+
+    /*
+    Edabit Challenge
+    https://edabit.com/challenge/hf2THAoQRQbAx2jc9
+
+    Write a function that finds the sum of an array. Make your function recursive.
+
+    Examples:
+    sum([1, 2, 3, 4]) ➞ 10
+
+    sum([1, 2]) ➞ 3
+
+    sum([1]) ➞ 1
+
+    sum([]) ➞ 0
+
+    Notes:
+    Return 0 for an empty array.
+    Check the Resources tab for info on recursion
+     */
+
+    public static int sum(int[] arr, int l) {
+
+        if (l <= 0)
+            return 0;
+
+        return (sum(arr, l - 1) + arr[l - 1]);
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {1, 2, 3, 4};
+        System.out.println(sum(numbers, numbers.length));
+    }
+}
