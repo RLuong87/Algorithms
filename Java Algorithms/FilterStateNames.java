e36b769fdd290b75715e8ea5b068bf9c58af0525
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2021-11-10 22:11:39 -0500

    Add files via upload

---
+++ Java Algorithms/FilterStateNames.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class FilterStateNames {
+
+    /*
+    Create a function that filters out an array of state names into two categories based on the second parameter.
+
+    Abbreviations abb
+    Full names full
+
+    Examples:
+    filterStateNames(["Arizona", "CA", "NY", "Nevada"], "abb")
+    ➞ ["CA", "NY"]
+
+    filterStateNames(["Arizona", "CA", "NY", "Nevada"], "full")
+    ➞ ["Arizona", "Nevada"]
+
+    filterStateNames(["MT", "NJ", "TX", "ID", "IL"], "abb")
+    ➞ ["MT", "NJ", "TX", "ID", "IL"]
+
+    filterStateNames(["MT", "NJ", "TX", "ID", "IL"], "full")
+    ➞ []
+
+    Notes:
+    State abbreviations will always be in uppercase.
+     */
+
+    public static String[] filterStateNames(String[] arr, String type) {
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+
+        String[] states = {"Arizona", "CA", "NY", "Nevada"};
+
+
+
+    }
+}
