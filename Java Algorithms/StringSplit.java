3e79d8c8098411478a4f08b8b26d599259fe7497
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2022-01-14 13:20:03 -0500

    Seperate vowels and consanants

--- Java Algorithms/StringSplit.java
+++ Java Algorithms/StringSplit.java
@@ -2,44 +2,42 @@ package com.company;
 
 public class StringSplit {
 
-    /*
-    Write a function that takes a string, breaks it up and returns it with vowels first, consonants second.
-    For any character that's not a vowel (like special characters or spaces), treat them like consonants.
+        /*
+        https://edabit.com/challenge/jfRHGwHMZcnzZm9JL
 
-    Examples:
-    split("abcde") ➞ "aebcd"
+        Write a function that takes a string, breaks it up and returns it with vowels first, consonants second.
+        For any character that's not a vowel (like special characters or spaces), treat them like consonants.
 
-    split("Hello!") ➞ "eoHll!"
+        Examples:
+        split("abcde") ➞ "aebcd"
 
-    split("What's the time?") ➞ "aeieWht's th tm?"
+        split("Hello!") ➞ "eoHll!"
 
-    Notes:
+        split("What's the time?") ➞ "aeieWht's th tm?"
 
-    Vowels are a, e, i, o, u.
-    Define a separate isVowel() function for easier to read code (recommendation).
-     */
+        Notes:
+        Vowels are a, e, i, o, u.
+        Define a separate isVowel() function for easier to read code (recommendation).
+         */
 
     public static String split(String str) {
+
 
         return "";
     }
 
+    public static String isVowel(String s) {
+        String vowel = "";
+
+
+
+        return vowel;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(split("abcde"));
-        System.out.println(split("Hello"));
-        System.out.println(split("What's the time?"));
+        System.out.println(isVowel("Hello"));
 
-        String str = "abcde", splitStr = "";
-
-        for (char c : str.toCharArray()) {
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' && Character.isUpperCase(c)) {
-                splitStr += c;
-            } else {
-                splitStr += c;
-            }
-        }
-        System.out.println(splitStr);
     }
 
 }
