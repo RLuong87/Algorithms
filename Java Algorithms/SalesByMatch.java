60d195c5477e8426d40ca06756403a44abb859a1
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2022-01-31 21:36:14 -0500

    Add files via upload

---
+++ Java Algorithms/SalesByMatch.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.List;
+
+public class SalesByMatch {
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
