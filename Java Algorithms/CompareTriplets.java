71b2a2a7ab44a844cf91e9674c9f5ad22b9161f3
Richies Luong <87341263+RLuong87@users.noreply.github.com>
2022-02-17 22:24:22 -0500

    Add files via upload

---
+++ Java Algorithms/CompareTriplets.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CompareTriplets {
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+
+        List<Integer> totalRatings = new ArrayList<>();
+
+        int aCount = 0, bCount = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+
+            if (a.get(i) > b.get(i)) {
+                aCount++;
+            } else if (b.get(i) > a.get(i)) {
+                bCount++;
+            }
+        }
+        totalRatings.add(aCount);
+        totalRatings.add(bCount);
+
+        return totalRatings;
+    }
+
+    public static void main(String[] args) {
+
+//        17 28 30
+//        99 16 8
+
+        List<Integer> aliceRating = new ArrayList<>(List.of(17, 28, 30));
+        List<Integer> bobRating = List.of(99, 16, 8);
+        List<Integer> totalRatings = new ArrayList<>();
+
+        System.out.println(compareTriplets(aliceRating, bobRating));
+
+        int aCount = 0, bCount = 0;
+
+        for (int i = 0; i < aliceRating.size(); i++) {
+
+            if (aliceRating.get(i) > bobRating.get(i)) {
+                aCount++;
+            } else if (bobRating.get(i) > aliceRating.get(i)) {
+                bCount++;
+            }
+        }
+        totalRatings.add(aCount);
+        totalRatings.add(bCount);
+
+        System.out.println(totalRatings);
+    }
+}
